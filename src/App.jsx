eb9b4f366ea3fa9775bf5d4f9a6c993732e95174
mehdi657 <mehdi657@gmail.com>
2023-03-27 22:57:51 +0200

    build

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route exact path="/" element={<Accueil />} />
+        <Route path="/" element={<Accueil />} />
         <Route path="/Projects" element={<Projects />} />
         <Route path="/APropos" element={<APropos />} />
       </Routes>
